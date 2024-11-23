d780eeb3671219087889c744b0018b859d878484
georrous6 <giorgosrousomanis23@gmail.com>
2024-11-24 01:38:51 +0200

    Fixed Armijo Rule

--- Assignment-2/armijo_rule.m
+++ Assignment-2/armijo_rule.m
@@ -1,25 +1,24 @@
-function steps = armijo_rule(beta, sigma, s, cf, grad_x_k, x_k, d_k)
+function gammas = armijo_rule(beta, alpha, s, cf, grad_x_k, x_k, d_k)
     % beta       - How fast the step size decreases
-    % sigma      - How much improvement we have at each step
+    % alpha      - How much improvement we have at each step
     % s          - The starting step
     % cf         - The cost function
     % grad_x_k   - The gradient of the cost function at the starting point
     % x_k        - The starting point
-    % d_k        - The directionto search for the appropriate step
+    % d_k        - The direction to search for the appropriate step
 
-    steps = NaN(1, 100);
-    i = 1;
-    step = s;
+    gammas = NaN(1, 100);
+    m = 0;
     f_k = cf(x_k(1), x_k(2));
     while true
-        f_k_plus_1 = cf(x_k(1) + step * d_k(1), x_k(2) + step * d_k(2));
-        steps(i) = step;
-        if f_k_plus_1 - f_k <= sigma * step * grad_x_k * d_k'
+        gamma = s * beta^m;
+        gammas(m + 1) = gamma;
+        f_k_plus_1 = cf(x_k(1) + gamma * d_k(1), x_k(2) + gamma * d_k(2));
+        if f_k_plus_1 - f_k <= alpha * gamma * grad_x_k * d_k'
             break;
         end
-        step = step * beta;
-        i = i + 1;
+        m = m + 1;
     end
 
-    steps = steps(~isnan(steps));
+    gammas = gammas(1:m+1);
 end
--- Assignment-2/steepest_descend.m
+++ Assignment-2/steepest_descend.m
@@ -18,7 +18,7 @@ ylabel('y');
 zlabel('z');
 title('Objective Function');
 
-tolerance = 1e-4;  % Gradient tolerance
+tolerance = 0.0015;  % Gradient tolerance
 X0 = [0, -1, 1];  % Starting x values
 Y0 = [0, 1, -1];  % Starting y values
 iters = [0, 0, 0];  % Holds the number of iterations
@@ -26,8 +26,8 @@ colors = {'r', 'g', 'y'};
 contourLevels = 30;
 
 % Save the paths for each starting point
-xpaths = NaN(3, 1000);
-ypaths = NaN(3, 1000);
+xpaths = NaN(3, 10000);
+ypaths = NaN(3, 10000);
 
 % Save the value of the cost function on each iteration
 costs = NaN(3, 1000);
@@ -41,7 +41,7 @@ switch scenario
 
     case 'fixed_step'
 
-        step = 0.5;  % Use fixed step
+        gamma = 0.0001;  % Use fixed step
         for i=1:length(X0)
             x = X0(i);
             y = Y0(i);
@@ -50,22 +50,23 @@ switch scenario
                 xpaths(i,iters(i)) = x;
                 ypaths(i,iters(i)) = y;
                 [dfdx, dfdy] = grad(x, y);
-                if (norm([dfdx, dfdy]) < tolerance)
+                grad_norm = norm([dfdx, dfdy]);
+                if (grad_norm < tolerance)
                     break;
                 end
-                x = x - step * dfdx;
-                y = y - step * dfdy;
+                x = x - gamma * dfdx / grad_norm;
+                y = y - gamma * dfdy / grad_norm;
             end
-            fprintf("Fixed step: (x0,y0)=(%f,%f), iterations: %d\n", X0(i), Y0(i), iters(i));
+            fprintf("Fixed step: (x0,y0)=(%d,%d), iterations: %d\n", X0(i), Y0(i), iters(i));
         end
-        titleString = sprintf('Gradient Descend Path (Fixed step: %.2f)', step);
+        titleString = sprintf('Gradient Descend Path (Fixed step: %.2f)', gamma);
 
     case 'line_minimization'
 
         % Define fibonacci parameters
         l = 0.001;
         e = 0.001;
-        scaleFactor = 5;
+        distance = 3;
         
         for i=1:length(X0)
             x = X0(i);
@@ -75,26 +76,26 @@ switch scenario
                 xpaths(i,iters(i)) = x;
                 ypaths(i,iters(i)) = y;
                 [dfdx, dfdy] = grad(x, y);
-                if (norm([dfdx, dfdy]) < tolerance)
+                grad_norm = norm([dfdx, dfdy]);
+                if (grad_norm < tolerance || iters(i) > 1000)
                     break;
                 end
-                xd = @(d) (x - d * dfdx);
-                yd = @(d) (y - d * dfdy);
+                xd = @(d) (x - d * dfdx / grad_norm);
+                yd = @(d) (y - d * dfdy / grad_norm);
                 zd = @(d) (objfunc(xd(d), yd(d)));
-                b = scaleFactor / norm([dfdx, dfdy]);
-                step = fibonacci_method(0, b, l, e, zd);
-                x = xd(step);
-                y = yd(step);
+                gamma = fibonacci_method(0, distance, l, e, zd);
+                x = xd(gamma);
+                y = yd(gamma);
 
                 % Visualize the first 3 iterations
                 if iters(i) <= 3 && i >= 2
                     subplot(2, 3, iters(i) + (i - 2) * 3);
-                    dvalues = linspace(0, b, 100);
+                    dvalues = linspace(0, distance, 100);
                     plot(dvalues, zd(dvalues), 'LineWidth', 1.5);
-                    title(sprintf('(x0,y0)=(%d,%d) Iter %d Step: %.2f', X0(i), Y0(i), iters(i), step));
+                    title(sprintf('(x0,y0)=(%d,%d) Iter %d Step: %.2f', X0(i), Y0(i), iters(i), gamma));
                 end
             end
-            fprintf("Line minimization: (x0,y0)=(%f,%f), iterations: %d\n", X0(i), Y0(i), iters(i));
+            fprintf("Line minimization: (x0,y0)=(%d,%d), iterations: %d\n", X0(i), Y0(i), iters(i));
         end
 
         titleString = 'Gradient Descend Path (Line Minimization)';
@@ -102,9 +103,9 @@ switch scenario
     case 'armijo_rule'
 
         % Define Armijo rule parameters
-        beta = 0.2;  % Denotes how fast the step size changes
-        sigma = 0.1;  % Denotes how much improvement do we demand at each step
-        scaleFactor = 5;
+        beta = 0.5;  % Denotes how fast the step size changes
+        aplha = 0.01;  % Denotes how much improvement do we demand at each step
+        s = 3;
         
         for i=1:length(X0)
             x = X0(i);
@@ -114,31 +115,32 @@ switch scenario
                 xpaths(i,iters(i)) = x;
                 ypaths(i,iters(i)) = y;
                 [dfdx, dfdy] = grad(x, y);
-                if (norm([dfdx, dfdy]) < tolerance)
+                grad_norm = norm([dfdx, dfdy]);
+                if (grad_norm < tolerance)
                     break;
                 end
-                s = scaleFactor / norm([dfdx, dfdy]); % Set the initial step
-                steps = armijo_rule(beta, sigma, s, @objfunc, [dfdx, dfdy], [x, y], [-dfdx, -dfdy]);
-                step = steps(end);
+                d_k = [-dfdx, -dfdy] / grad_norm;
+                gammas = armijo_rule(beta, aplha, s, @objfunc, [dfdx, dfdy], [x, y], d_k);
+                gamma = gammas(end);
                 % Visualize the first 3 iterations
                 if iters(i) <= 3 && i >= 2
                     dvalues = linspace(0, s, 100);
-                    cost_values = objfunc(x - dvalues * dfdx, y - dvalues * dfdy) - objfunc(x, y);
-                    threshold_line = -sigma * (dfdx^2 + dfdy^2) * dvalues;
-                    worst_case_line = -(dfdx^2 + dfdy^2) * dvalues;
+                    cost_values = objfunc(x + dvalues * d_k(1), y + dvalues * d_k(2)) - objfunc(x, y);
+                    threshold_line = aplha * [dfdx, dfdy] * d_k' * dvalues;
+                    worst_case_line = [dfdx, dfdy] * d_k' * dvalues;
                     subplot(2, 3, iters(i) + (i - 2) * 3);
                     plot(dvalues, cost_values, 'Color', 'b', 'LineWidth', 1.5);
                     hold on;
                     plot(dvalues, threshold_line, '--g', 'LineWidth', 1.5);
                     plot(dvalues, worst_case_line, '--m', 'LineWidth', 1.5);
-                    plot(steps, zeros(1, length(steps)), 'xr', 'MarkerSize', 10, 'LineWidth', 1.5);
-                    title(sprintf('(x0,y0)=(%d,%d) Iter %d Step: %.2f', X0(i), Y0(i), iters(i), step));
+                    plot(gammas, zeros(1, length(gammas)), 'xr', 'MarkerSize', 10, 'LineWidth', 1.5);
+                    title(sprintf('(x0,y0)=(%d,%d) Iter %d Step: %.2f', X0(i), Y0(i), iters(i), gamma));
                     hold off;
                 end
-                x = x - step * dfdx;
-                y = y - step * dfdy;
+                x = x - gamma * dfdx / grad_norm;
+                y = y - gamma * dfdy / grad_norm;
             end
-            fprintf("Armijo Rule: (x0,y0)=(%f,%f), iterations: %d\n", X0(i), Y0(i), iters(i));
+            fprintf("Armijo Rule: (x0,y0)=(%d,%d), iterations: %d\n", X0(i), Y0(i), iters(i));
         end
 
         titleString = 'Armijo Rule';
@@ -153,8 +155,8 @@ ylabel('Y-axis');
 title(titleString);
 colorbar; % Add a colorbar for reference
 for i=1:length(X0)
-    xpath = xpaths(i, ~isnan(xpaths(i, :)));
-    ypath = ypaths(i, ~isnan(ypaths(i, :)));
+    xpath = xpaths(i, 1:iters(i));
+    ypath = ypaths(i, 1:iters(i));
     plot(xpath, ypath, '--o', 'LineWidth', 1, 'Color', colors{i}, ...
         'DisplayName', sprintf('(x0,y0)=(%d,%d)', X0(i), Y0(i)));
 end
